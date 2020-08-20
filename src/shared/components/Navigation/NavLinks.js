d4a8b5974a683b1b9255fc8e4076525c4de46ab5
jakub_banasiak <soccerkid.bany@gmail.com>
2020-08-20 09:10:18 +0200

    end of module 10

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -6,37 +6,37 @@ import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
-  const auth = useContext(AuthContext);
+    const auth = useContext(AuthContext);
 
-  return (
-    <ul className="nav-links">
-      <li>
-        <NavLink to="/" exact>
-          ALL USERS
-        </NavLink>
-      </li>
-      {auth.isLoggedIn && (
-        <li>
-          <NavLink to="/u1/places">MY PLACES</NavLink>
-        </li>
-      )}
-      {auth.isLoggedIn && (
-        <li>
-          <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
-      )}
-      {!auth.isLoggedIn && (
-        <li>
-          <NavLink to="/auth">AUTHENTICATE</NavLink>
-        </li>
-      )}
-      {auth.isLoggedIn && (
-        <li>
-          <button onClick={auth.logout}>LOGOUT</button>
-        </li>
-      )}
-    </ul>
-  );
+    return (
+        <ul className="nav-links">
+            <li>
+                <NavLink to="/" exact>
+                    ALL USERS
+                </NavLink>
+            </li>
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/places/new">ADD PLACE</NavLink>
+                </li>
+            )}
+            {!auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/auth">AUTHENTICATE</NavLink>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>LOGOUT</button>
+                </li>
+            )}
+        </ul>
+    );
 };
 
 export default NavLinks;
